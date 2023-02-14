4fa19481acbbb29407321cc7f4ffea127dec84ae
ermusaelyan <e.musaelyan98@gmail.com>
2023-02-14 22:36:53 +0400

    add

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Promotions from './components/Promotions/Promotions';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header" />
+      <Promotions />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Promotions/Promotions.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Promotions = () => {
+  return (
+    <div className="promotions">
+      <div className="container">
+        <div className="promotions__banner">
+          <img src="" alt="promotions" />
+        </div>
+        <ul className="promotions__list">
+          <li className="promotions__item">
+            <a className="promotions__link" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Promotions;
