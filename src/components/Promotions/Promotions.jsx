465a999df4cec3f59a8dfffbc795f758744cacb0
ermusaelyan <e.musaelyan98@gmail.com>
2023-02-15 06:06:55 -0800

    upd

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PromotionsItem from '../PromotionsItem/PromotionsItem';
+import { useMediaQuery } from 'react-responsive';
 
 const Promotions = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 1020px)',
+  });
+
   return (
     <div className="promotions">
       <div className="container">
         <div className="promotions__banner">
+          {isMobile && (
+            <div className="promotions__backToMenu">
+              <button className="promotions__backToMenuButton">
+                <svg>
+                  <use xlinkHref={'#back'} />
+                </svg>
+              </button>
+            </div>
+          )}
           <div className="promotions__icon">
             <svg>
               <use xlinkHref={'#promotions'} />
